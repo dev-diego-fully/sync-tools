ce7eeda1c3c23494c457d3584d3842c6f69dc0d2
Diego Fully <dev.diegofully@gmail.com>
2025-08-09 17:51:12 -0300

    feat: Add ParallelFunction class and unpack_call utility
    This commit introduces two new features to the synctools library:
    
        ParallelFunction class: A new class that provides a mechanism to wrap a function call with a scoped lock on an arbitrary number of mutexes. This is useful for safely executing code that accesses multiple shared resources without the risk of deadlocks, as the locks are acquired simultaneously.
    
        unpack_call utility: A small, templated helper function that uses C++17 std::index_sequence and std::make_index_sequence to unpack a container (like std::vector) and pass its elements as arguments to a function. This utility is used by ParallelFunction to lock multiple mutexes in a single std::scoped_lock call.
    
    The existing Channel class was also slightly updated with minor comment formatting changes.

--- sync.hpp
+++ sync.hpp
@@ -3,8 +3,26 @@
 #include <condition_variable>
 #include <mutex>
 #include <queue>
+#include <utility>
 
 namespace synctools {
+
+// Internal implementation detail to unpack a container and call a function with its elements.
+// This function is not meant to be called directly.
+template <typename Func, typename C, std::size_t... Is>
+auto unpack_call_impl(Func&& func, const C& container,
+                      std::index_sequence<Is...>) -> decltype(auto) {
+  return std::forward<Func>(func)(container[Is]...);
+}
+
+// Public interface to unpack a container and call a function with its elements.
+// It deduces the container's size and generates the index sequence.
+template <typename Func, typename C>
+auto unpack_call(Func&& func, const C& container) -> decltype(auto) {
+  return unpack_call_impl(std::forward<Func>(func), container,
+                          std::make_index_sequence<container.size()>{});
+}
+
 
 /**
  * @brief A thread-safe, generic channel implementation.
@@ -19,9 +37,11 @@ class Channel {
  private:
   // Mutex to protect shared resources (the buffer).
   std::mutex read_write_lock;
-  // Condition variable to signal that the buffer is not empty and can be read from.
+  // Condition variable to signal that the buffer is not empty and can be read
+  // from.
   std::condition_variable can_read;
-  // Condition variable to signal that the buffer is not full and can be written to.
+  // Condition variable to signal that the buffer is not full and can be written
+  // to.
   std::condition_variable can_write;
   // The underlying buffer to store the elements.
   std::queue<T> buffer;
@@ -82,4 +102,55 @@ class Channel {
     return popped;
   }
 };
+
+/**
+ * @brief A class that wraps a function call with a scoped lock on multiple mutexes.
+ *
+ * It uses template metaprogramming to lock a variable number of mutexes
+ * provided in a container before calling the wrapped function.
+ *
+ * @tparam Func The type of the function to be wrapped.
+ * @tparam Return The return type of the function.
+ * @tparam Args The types of the arguments passed to the function.
+ */
+template <typename Func, typename Return, typename... Args>
+class ParallelFunction {
+  // Alias for a shared pointer to a mutex.
+  using SPtrMutex = std::shared_ptr<std::mutex>;
+
+ private:
+  // A vector of shared pointers to mutexes to be locked.
+  std::vector<SPtrMutex> mutexes;
+  // The function to be executed after the mutexes are locked.
+  Func function;
+
+ public:
+  /**
+   * @brief Constructs a new ParallelFunction object.
+   *
+   * @param mtxs A vector of shared pointers to mutexes.
+   * @param inner The function to be wrapped.
+   */
+  ParallelFunction(const std::vector<SPtrMutex>& mtxs, Func inner) {
+    this->mutexes = mtxs;
+    this->function = inner;
+  }
+
+  /**
+   * @brief The function call operator.
+   *
+   * It locks all mutexes in the `mutexes` vector and then
+   * calls the wrapped function with the provided arguments. The locks are
+   * automatically released when this function returns.
+   *
+   * @param args The arguments to forward to the wrapped function.
+   * @return The return value of the wrapped function.
+   */
+  Return operator()(Args&&... args) {
+    unpack_call([](auto&&... locks) { std::scoped_lock lock(locks...); },
+                this->mutexes);
+    return this->function(std::forward(args)...);
+  }
+};
+
 }  // namespace synctools
